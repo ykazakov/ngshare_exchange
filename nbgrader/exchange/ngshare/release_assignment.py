c2929644a58a3560c3c3bae5d3b2d8999b4288d1
aalmanza1998 <aalmanza@ucdavis.edu>
2020-03-04 10:05:56 -0800

    release assignment

--- nbgrader/exchange/ngshare/release_assignment.py
+++ nbgrader/exchange/ngshare/release_assignment.py
@@ -13,7 +13,6 @@ from nbgrader.exchange.abc import ExchangeReleaseAssignment as ABCExchangeReleas
 from nbgrader.exchange.ngshare import Exchange
 
 import requests
-import base64
 import json
 
 
@@ -38,34 +37,7 @@ class ExchangeReleaseAssignment(Exchange, ABCExchangeReleaseAssignment):
     def ensure_root(self):
         pass
 
-    def _encode_assignment(self):
-        assignment = []
-
-        for subdir, dirs, files in os.walk(self.src_path):
-            for filename in files:
-                filepath = subdir + os.sep + filename
-                data_read =  open(filepath, "r").read()
-                data_bytes = data_read.encode("utf-8")
-                encoded = base64.b64encode(data_bytes)
-                content = 'amtsCg==' + str(encoded)
-                file_map = {"path": filepath, "content": content}
-
-                assignment.append(file_map)
-        return assignment
-
-
-    def _post_assignment(self):
-        if self.coursedir.course_id == '':
-            self.fail("No course id specified. Re-run with --course flag.")
-        url = self.ngshare_url + '/api/assignment/{}/{}'.format(self.coursedir.course_id, self.coursedir.assignment_id)
-
-        data = {'files': json.dumps(self.assignment)}
-        r = requests.post(url = url, data = data)
-        self.log.info(r.content)
-
-
     def init_src(self):
-        self.ngshare_url = 'http://172.17.0.3:11111/' #need to get IP address of container
         self.src_path = self.coursedir.format_path(self.coursedir.release_directory, '.', self.coursedir.assignment_id)
 
         if not os.path.isdir(self.src_path):
@@ -79,13 +51,49 @@ class ExchangeReleaseAssignment(Exchange, ABCExchangeReleaseAssignment):
                     self.src_path,
                     self.coursedir.format_path(self.coursedir.release_directory, '.', '*'))
 
-        self.assignment = self._encode_assignment()
-        self._post_assignment()
+    '''
+    def _check_if_course_exists(self):
+        data = {'user': self.username}
+        list_url = self.ngshare_url + '/api/courses'
+        try:
 
+            response = requests.post(url = list_url, data = data)
+        except:
+            self.log.warm("Error occured while trying to list all courses")
+        self.log.info(response.json()['success'])
+        if response.json()['success']:
+            courses = response.json()['courses']
+
+            for course in courses:
+                if course == self.self.coursedir.course_id:
+                    return 
+
+            # if you got here couse doesn exist so create it
+            course_url = self.ngshare_url + '/api/course/{}'.format(self.coursedir.course_id)
+            response = requests.post(url = course_url, data = data)
+            self.log.info("created course")
+    '''
     def init_dest(self):
-        pass
-
+        data = {'user': self.username}
+        course_url = self.ngshare_url + '/api/course/{}'.format(self.coursedir.course_id)
+        response = requests.post(url = course_url, data = data)
+        #self._check_if_course_exists()
+        self.dest_path = self.ngshare_url + '/api/assignment/{}/{}'.format(self.coursedir.course_id, self.coursedir.assignment_id)
 
     def copy_files(self):
-        pass
 
+        
+        try:
+            data = self.encode_dir(self.src_path)
+            response = requests.post(url = self.dest_patht, data = data)
+        except:
+            self.log.warn('An error occurred while trying to release 2 {}'.format(self.coursedir.assignment_id))
+            
+        if response.status_code != requests.codes.ok:
+            self.log.warn('An error occurred while trying to release {}'.format(self.coursedir.assignment_id))  
+        elif not response.json()['success']:
+            self.log.warn('An error occurred while trying to release {}'.format(self.coursedir.assignment_id)) 
+            self.log.warn(response.json()['message'])
+        else:
+            self.log.info("Successfully released {}".format(self.coursedir.assignment_id))
+        
