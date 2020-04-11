245bc225b10e9d488bd580642bfbdf9e87a89f70
aalmanza1998 <aalmanza@ucdavis.edu>
2020-04-10 22:51:23 -0700

    add force flag

--- nbgrader/exchange/ngshare/release_assignment.py
+++ nbgrader/exchange/ngshare/release_assignment.py
@@ -45,13 +45,43 @@ class ExchangeReleaseAssignment(Exchange, ABCExchangeReleaseAssignment):
                 self._assignment_not_found(self.src_path, self.coursedir.format_path(self.coursedir.release_directory, '.', '*'))
 
     def init_dest(self):
+        if self.coursedir.course_id == '':
+            self.fail("No course id specified. Re-run with --course flag.")
         self.dest_path = '/assignment/{}/{}'.format(self.coursedir.course_id, self.coursedir.assignment_id)
 
-    def copy_files(self):
-        self.log.info('Encoding assignment')
-        data = self.encode_dir(self.src_path)
-        response = self.ngshare_api_post(self.dest_path, data)
+    def assignment_exists(self):
+        url = '/assignments/{}'.format(self.coursedir.course_id)
+        response = self.ngshare_api_get(url)
+        
         if response is None:
-            self.log.warn('An error occurred while trying to release {}'.format(self.coursedir.assignment_id))
-        else:
-            self.log.info('Successfully released {}'.format(self.coursedir.assignment_id))
+            self.log.error('An error occurred while trying to check if the assignment exists {}.'.format(self.coursedir.course_id))
+            return True
+
+        if self.coursedir.assignment_id in response['assignments']:
+            if self.force:
+                self.log.info("Overwriting files: {} {}".format(
+                    self.coursedir.course_id, self.coursedir.assignment_id
+                ))
+                delete_url = '/assignment/{}/{}'.format(self.coursedir.course_id, self.coursedir.assignment_id)
+                response = self.ngshare_api_delete(delete_url)
+                if response is None:
+                    self.fail('An error occurred while trying to delete {}'.format(self.coursedir.assignment_id))
+                    return True
+            else:
+                self.fail('Destination already exists, add --force to overwrite: {} {}'.format(
+                    self.coursedir.course_id, self.coursedir.assignment_id
+                ))
+                return True
+
+        return False
+
+
+    def copy_files(self):       
+        if not self.assignment_exists():
+            self.log.info('Encoding assignment')
+            data = self.encode_dir(self.src_path)
+            response = self.ngshare_api_post(self.dest_path, data)
+            if response is None:
+                self.log.warning('An error occurred while trying to release {}'.format(self.coursedir.assignment_id))
+            else:
+                self.log.info('Successfully released {}'.format(self.coursedir.assignment_id))
