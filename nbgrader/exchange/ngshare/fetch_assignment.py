4b19bc69d647824e5c669ccd681bc11186510b61
aalmanza1998 <aalmanza@ucdavis.edu>
2020-04-10 22:52:16 -0700

    add ignore option

--- nbgrader/exchange/ngshare/fetch_assignment.py
+++ nbgrader/exchange/ngshare/fetch_assignment.py
@@ -13,7 +13,7 @@ class ExchangeFetchAssignment(Exchange, ABCExchangeFetchAssignment):
 
     def _load_config(self, cfg, **kwargs):
         if 'ExchangeFetch' in cfg:
-            self.log.warning('Use ExchangeFetchAssignment in config, not ExchangeFetch. Outdated config:\n%s'
+            self.log.warninging('Use ExchangeFetchAssignment in config, not ExchangeFetch. Outdated config:\n%s'
                              ,
                              '\n'.join('ExchangeFetch.{key} = {value!r}'.format(key=key,
                              value=value) for (key, value) in
@@ -40,15 +40,24 @@ class ExchangeFetchAssignment(Exchange, ABCExchangeFetchAssignment):
         self.dest_path = os.path.abspath(os.path.join(self.assignment_dir, root))
         if os.path.isdir(self.dest_path) and not self.replace_missing_files:
             self.fail('You already have a copy of the assignment in this directory: {}'.format(root))
+    
+    def do_copy(self, files):
+        """Copy the src dir to the dest dir omitting the self.coursedir.ignore globs."""
+        if os.path.isdir(self.dest_path):
+            self.coursedir.ignore = True
+            self.decode_dir(files, self.dest_path, ignore=self.ignore_patterns())
+        else:
+            self.decode_dir(files, self.dest_path)
+        
+        self.log.info('Successfully decoded {}.'.format(self.coursedir.assignment_id))
 
     def copy_files(self):
         response = self.ngshare_api_get(self.src_path)
         if response is None:
-            self.log.warn('Failed to fetch assignment.')
+            self.log.warning('Failed to fetch assignment.')
         else:
             self.log.info('Successfully fetched {}. Will try to decode'.format(self.coursedir.assignment_id))
             try:
-                self.decode_dir(response['files'], self.dest_path)
-                self.log.info('Successfully decoded {}.'.format(self.coursedir.assignment_id))
+                self.do_copy(response['files'])
             except:
-                self.log.warn('Could not decode the assignment')
+                self.log.warning('Could not decode the assignment')
