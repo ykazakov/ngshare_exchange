fe4d69de71c5619867647e81e067917bcceac859
rkevin <rk@rkevin.dev>
2020-05-20 21:31:15 -0700

    add version and abstract out configureExchange

--- ngshare_exchange/__init__.py
+++ ngshare_exchange/__init__.py
@@ -8,27 +8,11 @@ from .fetch import ExchangeFetch
 from .fetch_assignment import ExchangeFetchAssignment
 from .collect import ExchangeCollect
 from .list import ExchangeList
-
-def configureExchange(c):
-    '''Modifies nbgrader configuration to use ngshare_exchange as the exchange.
-    c is the nbgrader config you get using get_config.
-    To use, simply specify the following in your nbgrader_config.py file:
-
-    import ngshare_exchage
-    ngshare_exchange.configureExchange(get_config())
-    '''
-    c.ExchangeFactory.exchange = Exchange
-    c.ExchangeFactory.fetch_assignment = ExchangeFetchAssignment
-    c.ExchangeFactory.fetch_feedback = ExchangeFetchFeedback
-    c.ExchangeFactory.release_assignment = ExchangeReleaseAssignment
-    c.ExchangeFactory.release_feedback = ExchangeReleaseFeedback
-    c.ExchangeFactory.list = ExchangeList
-    c.ExchangeFactory.submit = ExchangeSubmit
-    c.ExchangeFactory.collect = ExchangeCollect
+from .__version__ import __version__
+from .configure_exchange import configureExchange
 
 __all__ = [
     "Exchange",
-    "ExchangeError",
     "ExchangeCollect",
     "ExchangeFetch",
     "ExchangeFetchAssignment",
---
+++ ngshare_exchange/configure_exchange.py
@@ -0,0 +1,25 @@
+from .exchange import Exchange
+from .fetch_assignment import ExchangeFetchAssignment
+from .fetch_feedback import ExchangeFetchFeedback
+from .release_assignment import ExchangeReleaseAssignment
+from .release_feedback import ExchangeReleaseFeedback
+from .list import ExchangeList
+from .submit import ExchangeSubmit
+from .collect import ExchangeCollect
+
+def configureExchange(c):
+    '''Modifies nbgrader configuration to use ngshare_exchange as the exchange.
+    c is the nbgrader config you get using get_config.
+    To use, simply specify the following in your nbgrader_config.py file:
+
+    import ngshare_exchage
+    ngshare_exchange.configureExchange(get_config())
+    '''
+    c.ExchangeFactory.exchange = Exchange
+    c.ExchangeFactory.fetch_assignment = ExchangeFetchAssignment
+    c.ExchangeFactory.fetch_feedback = ExchangeFetchFeedback
+    c.ExchangeFactory.release_assignment = ExchangeReleaseAssignment
+    c.ExchangeFactory.release_feedback = ExchangeReleaseFeedback
+    c.ExchangeFactory.list = ExchangeList
+    c.ExchangeFactory.submit = ExchangeSubmit
+    c.ExchangeFactory.collect = ExchangeCollect
